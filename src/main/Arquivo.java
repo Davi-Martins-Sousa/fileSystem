1bca13e027957781d0ba08b21701f2d5dc33570f
Davi-Martins-Sousa <davi470063@gmail.com>
2023-08-22 12:15:17 -0300

    ls/cd/mkdir - final

---
+++ src/main/Arquivo.java
@@ -0,0 +1,42 @@
+package main;
+import java.time.LocalDateTime;
+
+public class Arquivo {
+    private String arquivoNome;
+    private Diretorio diretorioPai;
+    private LocalDateTime dataCriacao;
+    private String permicao;
+
+    // Getter and Setter
+    public String getArquivoNome() {
+        return arquivoNome;
+    }
+
+    public void setArquivoNome(String arquivoNome) {
+        this.arquivoNome = arquivoNome;
+    }
+
+    public Diretorio getDiretorioPai() {
+        return diretorioPai;
+    }
+
+    public void setDiretorioPai(Diretorio diretorioPai) {
+        this.diretorioPai = diretorioPai;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getPermicao() {
+        return permicao;
+    }
+
+    public void setPermicao(String permicao) {
+        this.permicao = permicao;
+    }
+}
--- src/main/MyKernel.java
+++ src/main/MyKernel.java
@@ -22,198 +22,284 @@ public class MyKernel implements Kernel {
     public MyKernel() {
 
         this.raiz = new Diretorio();
-        raiz.mkdir("/",null);
+        raiz.mkdir("/", null);
         raiz.setDiretorioPai(raiz);
         atualDiretorio = raiz;
     }
 
     public String ls(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: ls");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //System.out.println("ola pessoal!");
+        // inicio da implementacao do aluno
+        // System.out.println("ola pessoal!");
         result = atualDiretorio.ls(parameters, raiz);
-        //fim da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String mkdir(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: mkdir");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        Diretorio novoDiretorio = new Diretorio();
-        temporarioDiretorio = novoDiretorio.mkdir(parameters, atualDiretorio);
-        if(temporarioDiretorio == null){
-            result = "O nome do diretorio é inválido";
-        }
-        //fim da implementacao do aluno
-        return result;
-    }
+        // inicio da implementacao do aluno
+        String[] nomesDiretorio = new String[0];
+        Diretorio temporarioAtualDiretorio;
 
-    public String cd(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
-        String result = "";
-        //String currentDir = "";
-        System.out.println("Chamada de Sistema: cd");
-        System.out.println("\tParametros: " + parameters);
-
-        //inicio da implementacao do aluno
-        Diretorio antigoDiretorio = atualDiretorio;
-        atualDiretorio = atualDiretorio.cd(parameters);
-
-
-        if(atualDiretorio ==  antigoDiretorio){
-            result = "Diretório não existe.";
+        if ((parameters.startsWith("/"))) {
+            temporarioAtualDiretorio = raiz;
+            parameters = parameters.substring(1);
+            nomesDiretorio = parameters.split("/");
+        } else {
+            temporarioAtualDiretorio = atualDiretorio;
+            nomesDiretorio = parameters.split("/");
         }
 
-        else if (parameters.equals("..")) {
-            if (currentDir.equals("/")) {
-                result = "Não é possivel voltar mais!";
-            } else {
-                // Find the last index of '/' to go back one level.
-                int lastIndex = currentDir.lastIndexOf("/");
-                if (lastIndex != -1) {
-                    currentDir = currentDir.substring(0, lastIndex);
-                    if (currentDir.isEmpty()) {
-                        currentDir = "/";
+        for (String parte : nomesDiretorio) {
+            if (!parte.equals(".")) {
+                if ((parte.contains("/")) || (!parte.matches("^[A-Za-z0-9].*"))) {
+                    // throw new IllegalArgumentException("O nome do diretório não está em um
+                    // formato válido.");
+                    result = "Nome de diretório pode começar apenas com letras e números!";
+                }
+                if (parte == nomesDiretorio[nomesDiretorio.length - 1]) {
+                    for (Diretorio filho : temporarioAtualDiretorio.getDiretorioFilhos()) {
+                        if (filho.getDiretorioNome().equals(parte)) {
+                            result = parte + " já existe!";
+                        }
                     }
                 }
             }
         }
 
-        else{
-            if(currentDir.equals("/")){
-                currentDir = "";
+        if (result.equals("")) {
+            for (String parte : nomesDiretorio) {
+                boolean encontrado = false;
+                for (Diretorio filho : temporarioAtualDiretorio.getDiretorioFilhos()) {
+                    if (filho.getDiretorioNome().equals("..")) {
+                        temporarioAtualDiretorio = temporarioDiretorio.getDiretorioPai();
+                        encontrado = true;
+                        break;
+                    }
+                    if (filho.getDiretorioNome().equals(".")) {
+                        encontrado = true;
+                        break;
+                    }
+                    if (filho.getDiretorioNome().equals(parte)) {
+                        temporarioAtualDiretorio = filho;
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    Diretorio novoDiretorio = new Diretorio();
+                    temporarioAtualDiretorio = novoDiretorio.mkdir(parte, temporarioAtualDiretorio);
+                }
             }
-            currentDir = currentDir + "/" + parameters;
         }
 
-        //indique o diretório atual. Por exemplo... /
-        //currentDir = "/";
+        // fim da implementacao do aluno
+        return result;
+    }
 
-        //setando parte gráfica do diretorio atual
+    public String cd(String parameters) {
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
+        String result = "";
+        // String currentDir = "";
+        System.out.println("Chamada de Sistema: cd");
+        System.out.println("\tParametros: " + parameters);
+
+        // inicio da implementacao do aluno
+        String[] nomesDiretorio = new String[0];
+        Diretorio temporarioAtualDiretorio;
+
+        if(parameters.equals("/")){
+            atualDiretorio = raiz;
+            temporarioAtualDiretorio = raiz;
+        }else if ((parameters.startsWith("/"))) {
+            temporarioAtualDiretorio = raiz;
+            parameters = parameters.substring(1);
+            nomesDiretorio = parameters.split("/");
+        } else {
+            temporarioAtualDiretorio = atualDiretorio;
+            nomesDiretorio = parameters.split("/");
+        }
+
+        for (String parte : nomesDiretorio) {
+            boolean encontrado = false;
+            if (parte.equals("..")) {
+                temporarioAtualDiretorio = temporarioAtualDiretorio.getDiretorioPai();
+                encontrado = true;
+            } else if (parte.equals(".")) {
+                encontrado = true;
+            } else {
+                for (Diretorio filho : temporarioAtualDiretorio.getDiretorioFilhos()) {
+                    if (filho.getDiretorioNome().equals(parte)) {
+                        temporarioAtualDiretorio = filho;
+                        encontrado = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!encontrado) {
+                result = "caminho incorreto";
+                break;
+            }
+        }
+        if (result.equals("")) {
+            atualDiretorio = temporarioAtualDiretorio;
+        }
+
+        currentDir = "";
+        while (!temporarioAtualDiretorio.equals(raiz)) {
+            if(temporarioAtualDiretorio.equals(atualDiretorio)){
+                currentDir = temporarioAtualDiretorio.getDiretorioNome() + currentDir;
+            }
+            else{
+                currentDir = temporarioAtualDiretorio.getDiretorioNome() + "/" + currentDir;
+            }
+            temporarioAtualDiretorio = temporarioAtualDiretorio.getDiretorioPai();
+        }
+        currentDir = "/" + currentDir;
+
+        // indique o diretório atual. Por exemplo... /
+        // currentDir = "/";
+
+        // setando parte gráfica do diretorio atual
         operatingSystem.fileSystem.FileSytemSimulator.currentDir = currentDir;
 
-        //fim da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String rmdir(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: rmdir");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String cp(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: cp");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String mv(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: mv");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String rm(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: rm");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String chmod(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: chmod");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String createfile(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: createfile");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String cat(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: cat");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String batch(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: batch");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String dump(String parameters) {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: dump");
         System.out.println("\tParametros: " + parameters);
 
-        //inicio da implementacao do aluno
-        //fim da implementacao do aluno
+        // inicio da implementacao do aluno
+        // fim da implementacao do aluno
         return result;
     }
 
     public String info() {
-        //variavel result deverah conter o que vai ser impresso na tela apos comando do usuário
+        // variavel result deverah conter o que vai ser impresso na tela apos comando do
+        // usuário
         String result = "";
         System.out.println("Chamada de Sistema: info");
         System.out.println("\tParametros: sem parametros");
 
-        //nome do aluno
+        // nome do aluno
         String name = "Fulano da Silva";
-        //numero de matricula
+        // numero de matricula
         String registration = "2001.xx.yy.00.11";
-        //versao do sistema de arquivos
+        // versao do sistema de arquivos
         String version = "0.1";
 
         result += "Nome do Aluno:        " + name;
